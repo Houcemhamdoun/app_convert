7efd9b6ded1dee7dfa6a939c24c6f612b7652a4d
Houcemhamdoun <78470812+Houcemhamdoun@users.noreply.github.com>
2021-11-14 14:48:35 +0100

    Update convtemp.java

--- app/src/main/java/com/houssem/convert_temp/convtemp.java
+++ app/src/main/java/com/houssem/convert_temp/convtemp.java
@@ -1,10 +1,10 @@
 package com.houssem.convert_temp;
 
 public class convtemp {
-    public static double celsus_farenheit(double f){
+    public static double farenheit_celsus (double f){
         return(f-32)*5/9;
     }
-    public static double farenheit_celsus(double c){
+    public static double  celsus_farenheit (double c){
         return 32+c*9/5;
     }
 }
